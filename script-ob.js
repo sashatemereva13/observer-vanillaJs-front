598eb3cda3ecdd5caed34b0620eda91dc7a1c4e0
lostmart <martinpedraza1979@gmail.com>
2026-02-17 10:07:07 +0100

    feat: async fns, for loop with time intervals

---
+++ script-ob.js
@@ -0,0 +1,47 @@
+// ============================================
+// OBSERVER PATTERN IMPLEMENTATION
+// ============================================
+
+// 1. THE SUBJECT (The Broadcaster)
+// This button holds the 'state' of being clicked.
+const purchaseButton = document.querySelector("#buy-btn")
+
+// observers
+const observerOne = () => {
+	console.log("send a notification to the user")
+}
+const observerTwo = () => {
+	console.log("send  notification to the admin")
+}
+const observerThree = async () => {
+	const response = await fetch("https://jsonplaceholder.typicode.com/todos/1")
+	const json = await response.json()
+	console.log(json)
+}
+const observerFour = () => {
+	for (let i = 0; i < 10; i++) {
+		setTimeout(() => {
+			console.log(i, "send email to the user")
+		}, i * 1000)
+	}
+}
+const observerFive = () => {
+	console.log("send a text to the user")
+}
+const observerSix = () => {
+	console.log("send a text to the admin")
+}
+const observers = []
+
+// cannot change !
+const handlePurchaseClick = () => {
+	if (observers.length === 0) alert("No observers registered yet")
+
+	observers.forEach((observer) => observer())
+}
+
+// Attach our custom handler to the button
+
+purchaseButton.addEventListener("click", handlePurchaseClick)
+
+// add observers without changing handlePurchaseClick each time
